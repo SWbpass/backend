6627f1983fd61257cf3b0f460e70fcaee0cc2a52
always0ne <always0ne.dev@gmail.com>
2021-02-05 16:13:04 +0900

    refactor: update cors settings

--- src/main/java/com/bpass/backend/configs/WebMvcConfig.java
+++ src/main/java/com/bpass/backend/configs/WebMvcConfig.java
@@ -6,24 +6,14 @@ import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-/**
- * CORS 설정.
- *
- * @author always0ne
- * @version 1.0
- */
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedHeaders()
-        .allowedOrigins("*")
-        .allowedMethods(
-            HttpMethod.GET.name(),
-            HttpMethod.POST.name(),
-            HttpMethod.PUT.name(),
-            HttpMethod.DELETE.name())
+            .allowedHeaders()
+            .allowedOrigins("*")
+            .allowedMethods("*")
     ;
   }
 }
