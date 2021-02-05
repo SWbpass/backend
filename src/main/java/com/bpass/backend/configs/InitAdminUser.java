d8db55640086e85f46a9e173120825d2d2b8316c
always0ne <always0ne.dev@gmail.com>
2021-02-05 19:30:40 +0900

    chore: 테스트 계정 발급

--- src/main/java/com/bpass/backend/configs/InitAdminUser.java
+++ src/main/java/com/bpass/backend/configs/InitAdminUser.java
@@ -1,6 +1,8 @@
 package com.bpass.backend.configs;
 
 import com.bpass.backend.security.model.dto.SignUpAdminDto;
+import com.bpass.backend.security.model.dto.SignUpStoreDto;
+import com.bpass.backend.security.model.dto.SignUpUserDto;
 import com.bpass.backend.security.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
@@ -19,6 +21,21 @@ public class InitAdminUser implements ApplicationListener<ApplicationStartedEven
     public void onApplicationEvent(ApplicationStartedEvent applicationStartedEvent) {
         authService.signUpAdmin(
                 new SignUpAdminDto("admin@bpass.com", "password", "관리자", "전화번호", "집")
+        );
+
+        authService.signUpStore(
+                new SignUpStoreDto("test1@store.com", "password", "테스트가게 1", "010-1234-5678", "주소", "가게번호", "테스트 가게 1", "37.549469", "126.9694083 ")
+        );
+        authService.signUpStore(
+                new SignUpStoreDto("test2@store.com", "password", "테스트가게 2", "010-1234-5678", "주소", "가게번호", "테스트가게 2", "37.5513775", "126.9736404")
+        );
+
+        authService.signUpUser(
+                new SignUpUserDto("test1@user.com","password","테스트유저 1", "010-9876-4321", "집주소")
+        );
+
+        authService.signUpUser(
+                new SignUpUserDto("test2@user.com","password","테스트유저 2", "010-9876-4321", "집주소")
         );
     }
 }
